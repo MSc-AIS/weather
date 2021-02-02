ce45c55f1112b8bdcb8067108db3f7465f883caa
s_labrinos <slabrinos9214@gmail.com>
2021-02-02 20:40:07 +0200

    views lazy loading added to the application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback, lazy, Suspense } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,15 +6,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { authCheckState } from './store/actions';
 import './App.css';
 import { theme } from './shared/theme';
-
 import Layout from './hoc/Layout/Layout';
+import LoadingProgress from './components/UI/LoadingProgress/LoadingProgress';
 import CurrentPosition from './containers/CurrentPosition/CurrentPosition';
-import SearchCities from './containers/SearchCities/SearchCities';
-import Collections from './containers/Collections/Collections';
-import CityConditions from './components/CityConditions/CityConditions';
-import SignIn from './containers/Auth/SignIn';
-import SignUp from './containers/Auth/SignUp';
 import Logout from './containers/Auth/SignOut';
+
+//  Views lazy loading added. Stavros Labrinos [stalab at linuxmail.org] on 2/2/21.
+const SearchCities = lazy(() => import(`./containers/SearchCities/SearchCities`));
+const Collections = lazy(() => import(`./containers/Collections/Collections`));
+const CityConditions = lazy(() => import(`./components/CityConditions/CityConditions`));
+const SignIn = lazy(() => import(`./containers/Auth/SignIn`));
+const SignUp = lazy(() => import(`./containers/Auth/SignUp`));
 
 /**
  * @returns {JSX.Element}
@@ -36,17 +38,17 @@ function App() {
 
     const authRouting = isAuth ?
         <Switch>
-            <Route path="/collection" exact component={Collections} />
-            <Route path="/collection/:cityName" exact component={CityConditions} />
-            <Route path="/search" exact component={SearchCities} />
+            <Route path="/collection" render={props => <Collections {...props} />} />
+            <Route path="/collection/:cityName" render={props => <CityConditions {...props} />} />
+            <Route path="/search" render={props => <SearchCities {...props} />} />
             <Route path="/auth/sign-out" exact component={Logout} />
             <Route path="/" exact component={CurrentPosition} />
             <Redirect to="/" />
         </Switch> :
         <Switch>
-            <Route path="/search" exact component={SearchCities} />
-            <Route path="/auth/sign-in" exact component={SignIn} />
-            <Route path="/auth/sign-up" exact component={SignUp} />
+            <Route path="/search" render={props => <SearchCities {...props} />} />
+            <Route path="/auth/sign-in" render={props => <SignIn {...props} />} />
+            <Route path="/auth/sign-up" render={props => <SignUp {...props} />} />
             <Route path="/" exact component={CurrentPosition} />
             <Redirect to="/" />
         </Switch>;
@@ -55,7 +57,7 @@ function App() {
     <ThemeProvider theme={theme}>
         <div className="App">
             <Layout>
-                {authRouting}
+                <Suspense fallback={<LoadingProgress />}>{authRouting}</Suspense>
             </Layout>
         </div>
     </ThemeProvider>
