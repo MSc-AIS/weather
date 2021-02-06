e6c2262c65b2bc82261e421b29ded6dc6ccfedcf
s_labrinos <slabrinos9214@gmail.com>
2021-02-06 18:37:00 +0200

    changed localhost posts

--- src/axios-weather.js
+++ src/axios-weather.js
@@ -5,7 +5,7 @@ import axios from 'axios';
  */
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000/'
+    baseURL: 'https://msc-ais-weather:9000/'
 });
 
 export default instance;
